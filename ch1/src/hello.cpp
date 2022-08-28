074a41410b9c3677a1a195ae6ce8b52dafec6fbe
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2022-08-28 16:31:00 -0700

    First commit

---
+++ ch1/src/hello.cpp
@@ -0,0 +1,14 @@
+// Hello, world! program, for _C++ for Lazy Programmers_
+//  It prints "Hello, world!" on the screen
+//  Quite an accomplishment, huh?
+#include "SSDL.h"
+
+int main(int argc, char** argv)
+{
+    sout << "Hello, world!  (Press any key to quit.)\n";
+
+    SSDL_WaitKey();     // Wait for user to hit any key
+
+    return 0;
+}
+

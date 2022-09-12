942f35066831356e6be9f6de5ee6139fa87e6bfb
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2022-09-11 20:33:19 -0700

    Chapter 3

--- ch3/src/numbers.cpp
+++ ch3/src/numbers.cpp
@@ -1,4 +1,16 @@
+#include "SSDL.h">
+
 int main (int argc, char** argv)
 {
+    int seasonsOfAmericanIdol           = 18;
+    float hoursIveWatchedAmericanIdol   = 432.5F;
+    double howMuchIShouldCareAboutAmericanIdol = 1.0E-12;
+    double howMuchICareAboutAmericanIdol = 0.0000000001;
+
+    sout << "Through " << seasonsOfAmericanIdol
+    << " seasons of American Idol...\n";
+   
+    // end program
+    SSDL_WaitKey();
     return 0;
 }
